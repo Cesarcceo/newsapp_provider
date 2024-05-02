f85de896a9a37051faa05900e8e936b8c7e93a9c
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-05-02 11:16:28 -0600

    New feature (navegacion): :sparkles: la navegacion entre paginas ya es funcional

--- lib/src/pages/tabs_page.dart
+++ lib/src/pages/tabs_page.dart
@@ -1,27 +1,35 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
+import 'package:provider/provider.dart';
 
 
 class TabsPage extends StatelessWidget {
+  const TabsPage({super.key});
+
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _Paginas(),
-      bottomNavigationBar: _Navegacion(),
-   );
+    return ChangeNotifierProvider(
+      create: (_) => _NavegacionModel(),
+      child: const Scaffold(
+        body: _Paginas(),
+        bottomNavigationBar: _Navegacion(),
+         ),
+    );
   }
 }
 
 class _Navegacion extends StatelessWidget {
-  const _Navegacion({
-    super.key,
-  });
+  const _Navegacion();
 
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+    
     return BottomNavigationBar(
-      currentIndex: 0,
-      items: [
+      currentIndex: navegacionModel.paginaActual,
+      onTap: (index) => navegacionModel.paginaActual = index,
+      items: const [
         BottomNavigationBarItem(icon: Icon(Icons.person_outline), label: 'Para ti'),
         BottomNavigationBarItem(icon: Icon(Icons.public), label: 'Encabezados'),
       ]
@@ -30,15 +38,17 @@ class _Navegacion extends StatelessWidget {
 }
 
 class _Paginas extends StatelessWidget {
-  const _Paginas({
-    super.key,
-  });
+  const _Paginas();
 
   @override
   Widget build(BuildContext context) {
+
+    final navegacionModel = Provider.of<_NavegacionModel>(context);
+
     return PageView(
+      controller: navegacionModel.pageController,
       // physics: const BouncingScrollPhysics(),
-      physics: NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
       children: <Widget>[
         Container(
           color: Colors.red,
@@ -49,4 +59,19 @@ class _Paginas extends StatelessWidget {
       ],
     );
   }
+}
+
+class _NavegacionModel with ChangeNotifier{
+  int _paginaActual = 0;
+  final PageController _pageController = PageController();
+  
+  int get paginaActual => _paginaActual;
+
+  set paginaActual(int valor) {
+    _paginaActual = valor;
+    _pageController.animateToPage(valor, duration: const Duration(milliseconds:  250), curve: Curves.easeOut);
+    notifyListeners();
+  }
+
+  PageController get pageController => _pageController;
 }
--- lib/src/theme/tema.dart
+++ lib/src/theme/tema.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
 final miTema = ThemeData.dark().copyWith(
-  
+  bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+    selectedItemColor: Colors.red,
+    )
 );
