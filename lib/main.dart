da58eb52d92f3b91f6727d07292cea935fa382bc
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-05-02 10:26:31 -0600

    Initial commit :tada: inicio de la aplicacion de noticias

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp_provider/src/pages/pages.dart';
+import 'package:newsapp_provider/src/theme/tema.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      theme: miTema,
+      debugShowCheckedModeBanner: false,
+      home: TabsPage()
+    );
+  }
+}
---
+++ lib/src/pages/pages.dart
@@ -0,0 +1,4 @@
+
+export 'package:newsapp_provider/src/pages/tab1_page.dart';
+export 'package:newsapp_provider/src/pages/tab2_page.dart';
+export 'package:newsapp_provider/src/pages/tabs_page.dart';
