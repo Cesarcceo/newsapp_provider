9a4c61a0e28fe97451866ce0cba2270a20556145
Cesario54 <cesar.consuelo@cceo.com.mx>
2024-05-02 13:46:15 -0600

    New feature (mostrando noticias: diseño): :sparkles: las imagenes de las noticias ya se muestran en pantalla y se agregaron las descripciones de las noticias junto con dos botones debajo de la descrpcion

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+
 import 'package:newsapp_provider/src/models/models.dart';
-import 'package:newsapp_provider/src/theme/tema.dart';
 
 class ListaNoticias extends StatelessWidget {
 
   final List<Article> noticias;
 
-  const ListaNoticias({required this.noticias});
+  const ListaNoticias({super.key, required this.noticias});
 
   @override
   Widget build(BuildContext context) {
@@ -37,6 +37,13 @@ class _Noticia extends StatelessWidget {
         _TarjetaTitulo(noticia: noticia),
 
         _TarjetaImagen(noticia: noticia),
+
+        _TArjetaBody(noticia: noticia),
+
+        _TarjetaBotones(),
+
+        const SizedBox(height: 10),
+        const Divider(),
       ],
     );
   }
@@ -87,6 +94,58 @@ class _TarjetaImagen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('hola mundo');
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.all(Radius.circular(20)),
+          child: noticia.urlToImage != null
+          ? FadeInImage(
+            placeholder: const AssetImage('assets/giphy.gif'),
+            image: NetworkImage(noticia.urlToImage),
+          )
+          : const Image(image: AssetImage('assets/no-image.png'))
+      ),
+    );
+  }
+}
+
+class _TArjetaBody extends StatelessWidget {
+
+  final Article noticia;
+
+  const _TArjetaBody({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Text(noticia.description ?? '')
+    );
+  }
+}
+
+class _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.red,
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.star_border),
+        ),
+    
+        const SizedBox(width: 10),
+    
+        RawMaterialButton(
+          onPressed: () {},
+          fillColor: Colors.red,
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(20)),
+          child: const Icon(Icons.more),
+        ),
+      ],
+    );
   }
 }
